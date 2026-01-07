9e72108dc23448f24c743dd9294af5cf0fa92799
Aybüke Canöz <aykitap7@gmail.com>
2026-01-07 14:24:35 +0300

    feat: upgrade to .NET 9 and implement secure user authentication base

--- DevResourceAPI/Data/AppDbContext.cs
+++ DevResourceAPI/Data/AppDbContext.cs
@@ -4,12 +4,15 @@ using DevResourceAPI.Models;
 
 namespace DevResourceAPI.Data;
 
+
 public class AppDbContext : DbContext
 {
+    
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {}
     public DbSet<Resource> Resources { get; set; } = null!;
     public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
 {
